4636e610d4587bd66df5d8d65ca6790fa7ff78a4
sanimakusidi <31855549+sanimakusidi@users.noreply.github.com>
2026-07-07 05:56:08 +0100

    Comment out phone verification option in dashboard

--- dashboard.php
+++ dashboard.php
@@ -670,7 +670,7 @@ $balance = floatval($user['balance']);
                 <div class="verify-option-cost"><div class="verify-option-cost">NGN <?= number_format($costs['nin']) ?> per query</div>
 </div>
             </a>
-            <a href="verify.php?type=phone" class="verify-option">
+          <?php /* ?>  <a href="verify.php?type=phone" class="verify-option">
                 <div class="verify-option-icon">
                     <svg viewBox="0 0 24 24"><path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/></svg>
                 </div>
@@ -683,7 +683,7 @@ $balance = floatval($user['balance']);
                 </div>
                 <div class="verify-option-label">By Demographics</div>
                 <div class="verify-option-cost"><div class="verify-option-cost">NGN <?= number_format($costs['demographic']) ?> per query</div></div>
-            </a>
+            </a> <?php */ ?>
         </div>
     </div>
 
