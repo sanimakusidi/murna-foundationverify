8e6aeff5c8a43565ae74f65911f6697c2eabd122
sanimakusidi <31855549+sanimakusidi@users.noreply.github.com>
2026-09-17 12:12:25 +0100

    Comment out personal and premium slip download links
    Commented out links for Personal Info Slip and Premium NIN Slip.

--- verify.php
+++ verify.php
@@ -1642,9 +1642,10 @@ $fields = [
                     <a href="download_nin_pdf.php?format=verification" class="btn-new-verify">💳 Improved NIN Slip</a>
                     <a href="download_nin_pdf.php?format=premium" class="btn-new-verify">📄 Verification Page</a>
                     <a href="download_nin_pdf.php?format=card" class="btn-new-verify"> Regular NIN slip</a>
-                     <a href="download_personal_slip.php" class="btn-new-verify">📄 Personal Info Slip (PDF)</a>
-        <a href="download_premium_slip.php" class="btn-new-verify">⭐ Premium NIN Slip (PDF)</a>
-      
+                    <!--
+                                <a href="download_personal_slip.php" class="btn-new-verify">📄 Personal Info Slip (PDF)</a>
+                                <a href="download_premium_slip.php" class="btn-new-verify">⭐ Premium NIN Slip (PDF)</a>
+                      -->
                     <a href="verify.php" class="btn-new-verify">🔄 New Verification</a>
                 </div>
             </div>
